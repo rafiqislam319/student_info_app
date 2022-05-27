a1aaf4c20706e2dbffb5efd5fc33eac86dcb2b3f
rafiqislam319 <rafiqislam319@gmail.com>
2022-05-27 15:44:54 +0600

    implement subject seeder

---
+++ database/seeders/SubjectSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class SubjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('subjects')->insert([
+            'subject_name' => 'Bangla'
+        ]);
+        DB::table('subjects')->insert([
+            'subject_name' => 'English'
+        ]);
+    }
+}
